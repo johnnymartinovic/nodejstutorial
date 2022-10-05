334a4907f4c1c432af650fe7145e47591d1df653
Ivan Martinović <martinovic1412@gmail.com>
2022-10-05 17:11:27 -0400

    Playing with npm and node runs

--- hello_world.js
+++ hello_world.js
@@ -12,5 +12,5 @@ const server = app.listen(8081, function () {
     const host = server.address().address;
     const port = server.address().port;
 
-    console.log("App listening at http://%s:%s", host, port)
+    console.log("App listening at http://localhost:%s", port)
 });
